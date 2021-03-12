e564ffd2329ce43c18ccf8e6ccbef33401d8437b
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-12 00:23:42 -0700

    MultiProvider and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:news_provider/src/pages/tabs_page.dart';
+import 'package:news_provider/src/pages/web_view_page.dart';
+import 'package:news_provider/src/services/news_service.dart';
 import 'package:news_provider/src/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => NewsService())],
+      child: MaterialApp(
         title: 'Material App',
         theme: MyTheme.myTheme,
         debugShowCheckedModeBanner: false,
-        home: TabsPage());
+        home: TabsPage(),
+        initialRoute: 'tabspage',
+        routes: {
+          'tabspage': (BuildContext context) => TabsPage(),
+          'webview': (BuildContext context) => WebViewPage(),
+        },
+      ),
+    );
   }
 }
