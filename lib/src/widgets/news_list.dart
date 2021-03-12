cddffe792e67493a478394338f80d62c30ccf79b
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-12 00:25:40 -0700

    NewsList widget

---
+++ lib/src/widgets/news_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:news_provider/src/models/news_models.dart';
+import 'package:news_provider/src/widgets/news_card.dart';
+
+class NewsList extends StatelessWidget {
+  final List<Article> news;
+
+  const NewsList(this.news);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: news.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _News(
+            news: news[index],
+            index: index,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _News extends StatelessWidget {
+  final Article news;
+  final int index;
+
+  const _News({@required this.news, @required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return NewsCard(news: news, index: index);
+  }
+}
