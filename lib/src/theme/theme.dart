86178743649705db45fcb69322ce71398ceeeecd
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-12 00:25:17 -0700

    Poppins font family set

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyTheme {
-  static final myTheme =
-      ThemeData.dark().copyWith(accentColor: Colors.redAccent);
+  static final myTheme = ThemeData.dark().copyWith(
+    accentColor: Colors.redAccent,
+    textTheme: ThemeData.dark().textTheme.apply(
+          fontFamily: 'Poppins',
+        ),
+  );
 }
