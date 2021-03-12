9eca35c108d75e962125fe5305924a97aee210b6
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-11 19:06:39 -0700

    ChangeNotifierProvider, PageView animation

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(color: Colors.red),
-          Container(color: Colors.green),
-        ],
+    return ChangeNotifierProvider(
+      create: (_) => _NavigationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigation(),
       ),
     );
+  }
+}
+
+class _Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
+    return BottomNavigationBar(
+        onTap: (i) => navigationModel.currentPage = i,
+        currentIndex: navigationModel.currentPage,
+        items: [
+          BottomNavigationBarItem(
+              label: 'For you', icon: Icon(Icons.person_outline)),
+          BottomNavigationBarItem(label: 'Headlines', icon: Icon(Icons.public))
+        ]);
+  }
+}
+
+class _Pages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      controller: navigationModel.pageController,
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.green),
+      ],
+    );
+  }
+}
+
+class _NavigationModel with ChangeNotifier {
+  int _currentPage = 0;
+  PageController _pageController = PageController();
+
+  int get currentPage => _currentPage;
+  PageController get pageController => _pageController;
+
+  set currentPage(int value) {
+    this._currentPage = value;
+    _pageController.animateToPage(value,
+        duration: Duration(milliseconds: 250), curve: Curves.easeIn);
+    notifyListeners();
   }
 }
