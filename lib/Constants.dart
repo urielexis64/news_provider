6c0ae4bb40afe69c2e4651a77484b21f8056ef5e
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-12 21:45:33 -0700

    small constants added

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,5 +1,7 @@
 class Constants {
   static final double defaultPadding = 12.0;
+  static final double smallPadding = 6.0;
   static final double defaultMargin = 16.0;
+  static final double smallMargin = 8.0;
   static final double defaultBorderRadius = 12.0;
 }
