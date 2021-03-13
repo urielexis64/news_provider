130d03c4ddf5c482e15b9a92560cd76b83151b57
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-12 21:46:06 -0700

    changed to stateful widget to save the current state

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -3,16 +3,26 @@ import 'package:news_provider/src/services/news_service.dart';
 import 'package:news_provider/src/widgets/news_list.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Page extends StatelessWidget {
+class Tab1Page extends StatefulWidget {
   @override
+  _Tab1PageState createState() => _Tab1PageState();
+}
+
+class _Tab1PageState extends State<Tab1Page>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  // ignore: must_call_super
   Widget build(BuildContext context) {
     final newsService = Provider.of<NewsService>(context);
-    final headlines = newsService.headlines;
+    final articles = newsService.headlines;
     return Scaffold(
-        body: headlines.length == 0
+        body: articles.length == 0
             ? Center(
                 child: CircularProgressIndicator(),
               )
-            : NewsList(headlines));
+            : NewsList(articles));
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
